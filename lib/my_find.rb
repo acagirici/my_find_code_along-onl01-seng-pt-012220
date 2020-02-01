a8d084a4815599fc81d5df9ba4c55b629fc012f6
Arif Cagirici <acagirici23@gmail.com>
2020-02-01 20:40:44 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,12 +3,10 @@ require 'pry'
 def my_find(collection)
 	i = 0
 	  while i < collection.length
-	    block_return_values << yield(collection[i])
+	   if yield(collection[i])
+	      return
+	     collection[i]
+	   end  
 	    i = i + 1
 	  end
-	    if block_return_values.include?(false)
-	      false
-	    else
-	      true
-      end
 end
